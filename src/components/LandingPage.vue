05379fa11db3414fe4cb4c55f87b1be61acfeddd
MattThe10 <76494834+MattThe10@users.noreply.github.com>
2024-11-03 19:19:34 +0100

    First version

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="navbar">
+        
+    </div>
+    <div>
+        <div id="panel"></div>
+        <div id="content-page"></div>
+    </div>
+</template>
+
+<script>
+</script>
