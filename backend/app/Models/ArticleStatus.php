68ac99fcadb4ffa1c181b338a2921f2da9d739d4
Elika-V <ellashinkarenko1@gmail.com>
2025-01-07 22:08:14 +0100

    CON-39

--- backend/app/Models/ArticleStatus.php
+++ backend/app/Models/ArticleStatus.php
@@ -36,28 +36,4 @@ class ArticleStatus extends Model
 	{
 		return $this->hasMany(Article::class, 'article_statuses_id');
 	}
-
-	public function index(Request $request) 
-	{
-		// Initialize the query  
-		$article_statuses = ArticleStatus::query(); 
-			
-		//Check if there a 'search' parameter in the request 
-		if ($request->has('search') && $request->search !=	 null) {
-		
-			// Convert the search term to lowercase for case-insensitive matching 
-			$search = strtolower($request->search); 
-			
-			// Apply filtering based on the search term 
-			$article_statuses = $article_statuses->where(function ($query) use ($search) {
-				$query->whereRaw('LOWER(key) LIKE ?', ['%' . $search . '%']) // Filter by key
-					->orwhereRaw('LOWER(name) LIKE?', ['%' . $search . '%']); // Filter by name 
-			});
-		}	
-					
-		$article_statuses = $article_statuses->get(); 
-				
-		// Return the resulting article_statuses as a JSON response 
-		return response()->json($article_statuses);
-	}
 }
